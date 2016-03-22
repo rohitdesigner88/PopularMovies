9ee748c33efbcc147982e8d23a38f2e11fa8d74a
rohitdesigner88 <rohit.designer.88@gmail.com>
2016-03-22 05:51:18 +0530

    first commit

---
+++ app/src/main/java/com/rohit/udacity/Adapters/PosterAdapter.java
@@ -0,0 +1,56 @@
+package com.rohit.udacity.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.rohit.udacity.MainActivity;
+import com.rohit.udacity.R;
+import com.squareup.picasso.Picasso;
+
+
+public class PosterAdapter extends BaseAdapter {
+
+
+    private Context mContext;
+
+
+    public PosterAdapter(Context c) {
+        mContext = c;
+    }
+
+
+    public int getCount() {
+        return MainActivity.images.size();
+    }
+
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView poster;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            poster = (ImageView) inflater.inflate(R.layout.movie_poster, parent, false);
+        } else {
+            poster = (ImageView) convertView;
+        }
+
+        Picasso.with(mContext).load(MainActivity.images.get(position)).into(poster);
+
+        return poster;
+    }
+
+}
+
---
+++ app/src/main/java/com/rohit/udacity/connection/API.java
@@ -0,0 +1,11 @@
+package com.rohit.udacity.connection;
+
+public class API {
+
+    static public String API_URL = "http://api.themoviedb.org/3/discover/movie";
+    static public String API_KEY = "MY API KEY";
+    static public String IMAGE_URL = "http://image.tmdb.org/t/p/";
+    static public String IMAGE_SIZE_185 = "w185";
+    static public String IMAGE_NOT_FOUND = "http://i.imgur.com/N9FgF7M.png";
+
+}
